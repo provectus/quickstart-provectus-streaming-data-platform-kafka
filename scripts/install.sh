3f83d7d444f470cef244141ebb1533e38b0d9ebe
Andrew G. Saushkin <asaushkin@gmail.com>
2020-02-06 17:55:41 +0300

    Refactor VPC, MSK, ECS

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# This script is intended to deploy all templates by manually
+set -x
+
+# This script is intended to deploy all templates by hands
+# without using a "parent" stack.
 
 # ---- Configuration
-env=manual
-msk_instance_type=kafka.m5.large
+env=${1:-CreatedByScript}
+msk_instance_type=${2:-kafka.m5.large}
 
 # ---- Body
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -13,47 +16,27 @@ templates=${scripts}/../templates
 # Create VPC
 vpc_stack="VPC-${env}"
 
-aws cloudformation deploy --template-file $templates/vpc.template --stack-name $vpc_stack --parameter-overrides "EnvironmentName=$env"
+#aws cloudformation deploy --template-file $templates/vpc.template --stack-name $vpc_stack --parameter-overrides "EnvironmentName=$env"
 
 vpc_meta=$(aws cloudformation describe-stacks --stack-name $vpc_stack)
 vpc_id=$(echo $vpc_meta | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "VPC") | .OutputValue')
 vpc_private_subnets=$(echo $vpc_meta | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PrivateSubnets") | .OutputValue')
 vpc_public_subnets=$(echo $vpc_meta | jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "PublicSubnets") | .OutputValue')
 
-
-# Create security groups
-msk_sg_stack="MSK-SG-${env}"
-
-aws cloudformation deploy --template-file $templates/msk-sg.template --stack-name $msk_sg_stack \
-    --parameter-overrides "EnvironmentName=$env" "VPC=$vpc_id"
-
-msk_sg_meta=$(aws cloudformation describe-stacks --stack-name $msk_sg_stack)
-msk_sg_security_group_id=$(echo $msk_sg_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MSKSecurityGroupID") | .OutputValue')
-
-# Create MSK Cluster
+## Create MSK Cluster
 msk_stack="MSK-${env}"
-aws cloudformation deploy --template-file $templates/msk.template --stack-name $msk_stack \
-    --parameter-overrides "EnvironmentName=$env" "VPC=$vpc_id" "ClientSubnets=${vpc_private_subnets}" \
-        "InstanceType=$msk_instance_type" "MSKSecurityGroupID=$msk_sg_security_group_id"
+#aws cloudformation deploy --template-file $templates/msk.template --stack-name $msk_stack \
+#    --capabilities CAPABILITY_IAM \
+#    --parameter-overrides "EnvironmentName=$env" "VPC=$vpc_id" "PrivateSubnets=${vpc_private_subnets}" \
+#    "InstanceType=$msk_instance_type"
 
-msk_meta=$(aws cloudformation describe-stacks --stack-name $msk_stack)
-msk_arn=$(echo $msk_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MSKClusterArn") | .OutputValue')
-
-msk_info_stack="MSK-Info-${env}"
-aws cloudformation deploy --template-file $templates/describe-kafka.template --stack-name $msk_info_stack \
-    --parameter-overrides "EnvironmentName=$env" "MskArn=$msk_arn" \
-    --capabilities CAPABILITY_IAM
-
-msk_info_meta=$(aws cloudformation describe-stacks --stack-name $msk_info_stack)
-#msk_info_brokers=$(echo $msk_info_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MskBrokers") | .OutputValue')
-#msk_info_brokers_tls=$(echo $msk_info_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MskBrokersTls") | .OutputValue')
+#msk_meta=$(aws cloudformation describe-stacks --stack-name $msk_stack)
+#msk_arn=$(echo $msk_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MSKClusterArn") | .OutputValue')
+#msk_info_brokers=$(echo $msk_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MskBrokers") | .OutputValue')
+#msk_info_brokers_tls=$(echo $msk_meta |jq -r '.Stacks[0].Outputs[] | select(.OutputKey == "MskBrokersTls") | .OutputValue')
 
 ecs_stack="ECS-${env}"
-aws cloudformation deploy --template-file $templates/ecs.template --stack-name $ecs_stack \
-    --capabilities CAPABILITY_IAM \
-    --parameter-overrides "EnvironmentName=$env" "VPC=$vpc_id"
-
-
-alb_stack="ALB-${env}"
-aws cloudformation deploy --template-file $templates/alb.template --stack-name $alb_stack \
-    --parameter-overrides "EnvironmentName=$env" "VPC=$vpc_id" "VpcPublicSubnets=${vpc_public_subnets}"
+aws cloudformation deploy --template-file $templates/ecs-fargate.template --stack-name $ecs_stack \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides "VPC=$vpc_id" \
+    "PrivateSubnets=${vpc_private_subnets}" "PublicSubnets=${vpc_public_subnets}"
